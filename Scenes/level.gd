415739a21709ccf5876f1ee9b9788b9e52a57514
Little-Motivated <svetoslavkor@gmail.com>
2024-09-11 20:11:39 +0300

    Buttons work now

--- Scenes/level.gd
+++ Scenes/level.gd
@@ -10,7 +10,8 @@ var board = [[-1,-1,-1,-1,-1,-1,-1,-1,-1],
 			 [-1,-1,-1,-1,-1,-1,-1,-1,-1],
 			 [-1,-1,-1,-1,-1,-1,-1,-1,-1]]
 
-var active = [-1,-1]
+# var active = [-1,-1]
+var active
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -40,9 +41,54 @@ func _input(event: InputEvent) -> void:
 				yy = (yy - 66) / 70;
 				print(xx);
 				print(yy);
-				active = [yy,xx]
+				var num = yy * 9 + xx + 1
+				active = get_node("Numbers/" + str(num))
+				print(active)
+				print(get_node("Numbersdvsa"))
 
 
-func _on__pressed() -> void:
-	
-	
+
+func _on__pressed_1() -> void:
+	if is_instance_valid(active):
+		active.text = " 1 "
+		print("asdaf")
+
+
+func _on__pressed_2() -> void:
+	if is_instance_valid(active):
+		active.text = " 2 "
+
+
+func _on__pressed_3() -> void:
+	if is_instance_valid(active):
+		active.text = " 3 "
+
+
+func _on__pressed_4() -> void:
+	if is_instance_valid(active):
+		active.text = " 4 "
+
+
+func _on__pressed_5() -> void:
+	if is_instance_valid(active):
+		active.text = " 5 "
+
+
+func _on__pressed_6() -> void:
+	if is_instance_valid(active):
+		active.text = " 6 "
+
+
+func _on__pressed_7() -> void:
+	if is_instance_valid(active):
+		active.text = " 7 "
+
+
+func _on__pressed_8() -> void:
+	if is_instance_valid(active):
+		active.text = " 8 "
+
+
+func _on__pressed_9() -> void:
+	if is_instance_valid(active):
+		active.text = " 9 "
